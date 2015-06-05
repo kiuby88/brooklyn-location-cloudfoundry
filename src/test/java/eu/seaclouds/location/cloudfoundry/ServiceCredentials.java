8f1e8b399afdae4aedc3e1061a1b6671b9ef40ad
Kiuby88 <kiuby88.programming@gmail.com>
2015-06-05 09:27:51 +0200

    FIX: Test was adapted to any CloudFoundry PaaS Provides.

--- src/test/java/eu/seaclouds/location/cloudfoundry/ServiceCredentials.java
+++ src/test/java/eu/seaclouds/location/cloudfoundry/ServiceCredentials.java
@@ -25,62 +25,60 @@ import brooklyn.entity.proxying.EntitySpec;
 import brooklyn.test.Asserts;
 import com.google.common.collect.ImmutableList;
 import org.cloudfoundry.client.lib.domain.CloudServiceInstance;
+import org.cloudfoundry.client.lib.domain.CloudServiceOffering;
+import org.cloudfoundry.client.lib.domain.CloudServicePlan;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.testng.Assert.assertNotNull;
 import static org.testng.Assert.assertTrue;
 
 public class ServiceCredentials extends AbstractCloudFoundryPaasLocationLiveTest  {
 
-    public final String SERVICE_TYPE_FIELD = "ID" ;
-    static final String SERVICE_PLAN_FIELD = "PLAN";
-    static final String SERVICE_NAME_FIELD =  "NAME";
 
     @DataProvider(name = "serviceDescriptionProvider")
-    public Object[][] createServiceDescriptions() {
-        return new Object[][]{
-                {addServiceDescription("blazemeter", "free-tier"),},
-                {addServiceDescription("rediscloud", "30mb")},
-                {addServiceDescription("cleardb", "spark")},
-                {addServiceDescription("cloudamqp", "lemur")},
-                {addServiceDescription("elephantsql", "turtle")},
-                {addServiceDescription("sendgrid", "free")},
-                {addServiceDescription("mongolab", "sandbox")},
-                {addServiceDescription("newrelic", "standard")},
-                {addServiceDescription("loadimpact", "lifree")},
-                {addServiceDescription("memcachier", "dev")},
-                {addServiceDescription("memcachedcloud", "30mb")},
-                {addServiceDescription("searchly", "starter")},
-                {addServiceDescription("cloudforge", "free")},
-                {addServiceDescription("ironworker", "free")},
-                {addServiceDescription("ironmq", "free")},
-                {addServiceDescription("pubnub", "free")},
-                {addServiceDescription("cine-io", "starter")},
-                {addServiceDescription("cedexisradar", "free-community-edition")},
-                {addServiceDescription("statica", "starter")}
+    private Object[][] ServiceOfferingSupplier() throws Exception {
 
-                //{addServiceDescription("searchify", "")}, not free plan
-                //{addServiceDescription("cedexisopenmix", "")}, not free plan
-                //{addServiceDescription("temporize", "")}, not free plan
-        };
+        Object[][] freeServicesResult;
+
+        super.setUp();
+        cloudFoundryPaasLocation.setUpClient();
+        List<Object[]> freeServiceOffering = getFreeServiceOfferings(cloudFoundryPaasLocation
+                .getCloudFoundryClient()
+                .getServiceOfferings());
+
+        freeServicesResult = new Object[freeServiceOffering.size()][];
+        freeServiceOffering.toArray(freeServicesResult);
+        return freeServicesResult;
     }
 
-    private Map<String, String> addServiceDescription(String type, String plan){
-        String name = "test-brooklyn-"+type;
-        return addServiceDescription(type, name, plan);
+    private List<Object[]> getFreeServiceOfferings(List<CloudServiceOffering> serviceOfferings){
+        ArrayList<Object[]> list = new ArrayList<Object[]>();
+        for(CloudServiceOffering ser: serviceOfferings){
+            if(hasFreePlan(ser)){
+                    list.add(new Object[]{ser});
+            }
+        }
+        return list;
     }
 
-    private Map<String, String> addServiceDescription(String type, String name, String plan){
-        Map<String, String> service= new HashMap<String, String>();
-        service.put(SERVICE_TYPE_FIELD, type);
-        service.put(SERVICE_PLAN_FIELD, plan);
-        service.put(SERVICE_NAME_FIELD, name);
-        return service;
+    private boolean hasFreePlan(CloudServiceOffering serviceOffering){
+        return findFreePlan(serviceOffering)!=null;
+    }
+
+    private String findFreePlan(CloudServiceOffering serviceOffering){
+        String freePlan=null;
+
+        for(CloudServicePlan plan: serviceOffering.getCloudServicePlans()) {
+            if(plan.isFree()){
+                return plan.getName();
+            }
+        }
+        return freePlan;
     }
 
     @AfterMethod(alwaysRun = true)
@@ -89,84 +87,33 @@ public class ServiceCredentials extends AbstractCloudFoundryPaasLocationLiveTest
     }
 
     @Test(groups = {"Live"}, dataProvider = "serviceDescriptionProvider")
-    protected void instanceServiceTest(Map<String, String > serviceDescription) {
+    protected void instanceServiceTest2(CloudServiceOffering serviceOffering) {
 
-        final CloudFoundryService service = app.createAndManageChild(EntitySpec.create(GenericService.class)
-                .configure("serviceInstanceName", serviceDescription.get(SERVICE_NAME_FIELD))
-                .configure("plan", serviceDescription.get(SERVICE_PLAN_FIELD))
-                .configure("serviceType", serviceDescription.get(SERVICE_TYPE_FIELD))
-                .location(cloudFoundryPaasLocation));
+        String serviceInstanceName= "test-brooklyn-"+serviceOffering.getName();
+        String freePlan = findFreePlan(serviceOffering);
+        String serviceTypeId = serviceOffering.getName();
+
+        final CloudFoundryService service = app
+                .createAndManageChild(EntitySpec.create(GenericService.class)
+                        .configure("serviceInstanceName", serviceInstanceName)
+                        .configure("plan", freePlan)
+                        .configure("serviceType", serviceTypeId)
+                        .location(cloudFoundryPaasLocation));
 
         app.start(ImmutableList.of(cloudFoundryPaasLocation));
 
         final CloudServiceInstance serviceInstance = cloudFoundryPaasLocation
                 .getCloudFoundryClient()
-                .getServiceInstance(serviceDescription.get(SERVICE_NAME_FIELD));
+                .getServiceInstance(serviceInstanceName);
 
         Asserts.succeedsEventually(new Runnable() {
             @Override
             public void run() {
                 assertNotNull(serviceInstance);
                 assertTrue(serviceInstance.getCredentials().isEmpty());
-
             }
         });
     }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        /*
-
-        static {
-            HashMap<String, Map<String, String>> aMap = new HashMap<String, Map<String, String>>();
-            serviceDescriptions = Collections.unmodifiableMap(aMap);
-
-            //not free
-            //addServiceDescription("searchify", "test-Brooklyn-searchify", "");
-
-            //free
-            addServiceDescription("blazemeter","test-brooklyn-blazemeter", "free-tier");
-
-            //not
-            addServiceDescription("rediscloud","test-brooklyn-", "30mb");
-
-            //free
-            addServiceDescription("cleardb","test-brooklyn-cleardb", "spark");
-
-            //
-            addServiceDescription("cloudamqp","test-brooklyn-cloudamqp", "lemur");
-
-            //
-            //addServiceDescription("","test-brooklyn-", "");
-
-
-            //
-            //addServiceDescription("","test-brooklyn-", "");
-
-
-        }
-    */
-
-
-
-
-//    public static void addServiceDescription(String type, String name, String plan){
-//        Map<String, String> service= new HashMap<String, String>();
-//        service.put(SERVICE_TYPE_FIELD, type);
-//        service.put(SERVICE_PLAN_FIELD, plan);
-//        service.put(SERVICE_NAME_FIELD, name);
-//        serviceDescriptions.put(type, service);
-
+    
 }
